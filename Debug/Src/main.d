91675e68677eda360f40829c8dac04169f21d124
shinlnh <kusin0412@gmail.com>
2024-11-13 21:18:29 +0700

    init project

---
+++ Debug/Src/main.d
@@ -0,0 +1,5 @@
+Src/main.o: ../Src/main.c \
+ D:/STM32IDE/MCU1/005DriverSTM32F429xx/drivers/Inc/stm32f429xx_gpio_driver.h \
+ D:/STM32IDE/MCU1/005DriverSTM32F429xx/drivers/Inc/stm32f429xx.h
+D:/STM32IDE/MCU1/005DriverSTM32F429xx/drivers/Inc/stm32f429xx_gpio_driver.h:
+D:/STM32IDE/MCU1/005DriverSTM32F429xx/drivers/Inc/stm32f429xx.h:
